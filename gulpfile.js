7578397233f58ffe854eb7e12cdac56b494889d2
user601 <artemlisicin55@gmail.com>
2021-05-23 14:17:39 +0300

    hdfhkdfh

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,7 @@ return src('app/scss/style.scss')
 function scripts(){
 
   return src(['node_modules/jquery/dist/jquery.js',
+   'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'])
 
   .pipe(concat('main.min.js'))
